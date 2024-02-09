037e672fce03e9d0752df47b97cef4ab953afcf4
Rashid Harvey <rosk.abed@gmail.com>
2024-02-09 03:47:10 +0200

    added tokenization test

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
+from arab_tools import tokenize_with_location
 from trans import transliterate, Profile
+
+
+def test_tokenization():
+    text = "يَكْتُبُ الكَلْبُ"
+    tokens, starts, ends = tokenize_with_location(text)
+    assert tokens == ("يَكْتُبُ", "الكَلْبُ")
+    assert starts == (0, 9)
+    assert ends == (8, 17)
 
 
 def test_sun_assimilation():
@@ -9,4 +18,4 @@ def test_sun_assimilation():
         assert transliterate(arab, profile) == latin
 
 
-test_sun_assimilation()
+# test_sun_assimilation()
