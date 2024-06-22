4ef84be67cc50f187438017d39a9a32ad04f40cf
theRealProHacker <77074862+theRealProHacker@users.noreply.github.com>
2024-06-22 15:14:04 +0200

    Added gradio client for vocalisation

--- vocalization.py
+++ vocalization.py
@@ -5,7 +5,12 @@ Module for vocalization of Arabic text
 # Using mishkal
 # See: https://github.com/linuxscout/mishkal?tab=readme-ov-file#example
 
+# import json
+
 import mishkal.tashkeel
+
+# import requests
+import data
 
 vocalizer = mishkal.tashkeel.TashkeelClass("")
 
@@ -16,10 +21,33 @@ def vocalize(text: str) -> str:
     return o
 
 
+# This doesn't work
+# def vocalize(text: str) -> str:
+#     task = "diacritization"
+#     data = {"text": text, "task": task, "API_KEY": "lpcsTkDIDf"}
+#     response = requests.post(
+#         "https://farasa.qcri.org/diacritization/analyze/",
+#         data=data,
+#     )
+#     response.raise_for_status()
+#     return json.loads(response.text)["text"]
+
+
+def vocalize(text: str) -> str:
+    from gradio_client import Client
+
+    client = Client(
+        "https://testingdoang-shakkala-arabic-tashkeel.hf.space/--replicas/1eim7/"
+    )
+    return client.predict(text, api_name="/predict")
+
+
 # fix asmai from here
 
 import asmai.semdictionary  # noqa
 
 asmai.semdictionary.SemanticDictionary.lookup_rule = (
-    lambda self, primate_word, second_word: False
+    lambda self, primate_word, second_word: data.sem_relations.get(
+        (primate_word, second_word), False
+    )
 )
